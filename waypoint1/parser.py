b02d9ddc1a340a5a3396fad64e7b5641a93c25d7
Trân Dương <dtran2108@gmail.com>
2019-02-27 07:01:28 +0700

    require argument for waypoint1 do_argparse

--- waypoint1/parser.py
+++ waypoint1/parser.py
@@ -4,7 +4,8 @@ from argparse import ArgumentParser
 def do_argparse():
     """ do the argparse and return the path argument """
     parser = ArgumentParser()
-    parser.add_argument('--path', help='The root directory to'
+    parser.add_argument('--path', required=True,
+                        help='The root directory to'
                         ' start scanning for duplicate files.')
     args = parser.parse_args()
     path = args.path
